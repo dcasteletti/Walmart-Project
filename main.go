31326c9bb1779201746eccfa13a8c76b9c5592f1
Denisse Casteletti <dacasteletti@falabella.cl>
2021-10-29 11:32:51 -0300

    main.go completed

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/jessevdk/go-flags"
+	"log"
 	"net/http"
 	"proyect/handler"
 )
 
-func main()  {
-	router := mux.NewRouter()
-	router.HandleFunc("/products/list", handler.GetAllProducts).Methods(http.MethodGet)
-	router.HandleFunc("/products/{id}", handler.GetProducts).Methods(http.MethodGet)
+type settings struct {
+	Port string `short:"p" long:"port" default:"3001" description:"http server port"`
+}
 
-	http.ListenAndServe(":12345", router)
+func main() {
+	log.Print("server starting \n")
+
+	var st settings
+	parser := flags.NewParser(&st, flags.Default)
+
+	_, err := parser.Parse()
+	if errType, ok := err.(*flags.Error); ok && errType.Type == flags.ErrHelp {
+		return
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/v1/products/list", handler.GetAllProducts).Methods(http.MethodGet)
+	router.HandleFunc("/v1/products/{id}", handler.GetProducts).Methods(http.MethodGet)
+
+	err = http.ListenAndServe(fmt.Sprintf(":%s", st.Port), router)
+
+	fmt.Printf("service terminated %s", err)
 }
